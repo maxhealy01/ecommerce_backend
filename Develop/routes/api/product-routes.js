4df79e162b8c10c8838dedf2716ae27d3b643a78
Max Healy <maxhealy01@gmail.com>
2021-01-06 19:19:34 -0600

    Created models for products, tags, producttags, and categories. Created category routes.

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,6 +5,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
+  Product.findAll({
+    attributes: [
+      'id', 'product_name', 'price', 'stock'
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  })
   // find all products
   // be sure to include its associated Category and Tag data
 });
